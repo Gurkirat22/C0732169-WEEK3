4c8c1dfff66d384bf52ba70e2b24a8b3f92f020d
visualstudio <visualstudio@122-PC07>
2019-01-25 21:24:13 -0500

    Current Village

--- C0732169/Program.cs
+++ C0732169/Program.cs
@@ -13,18 +13,49 @@ namespace C0732169
 
         }
     }
-    class countryside
+    class Countryside
     {
-      
+        // how will I make some Villages?
+        Village Alst;
+        Village Maeland;
+        Village Schenig;
+
+        public void InitializeMap()
+        {
+            Alst = new Village();
+            Alst.VillageName = "Alst";
+            Maeland = new Village();
+            Maeland.VillageName = "Maeland";
+            Maeland.isAstrildeHere = true;
+            Schenig = new Village();
+            Schenig.VillageName = "Schenig";
+
+            Alst.NextVillage = Maeland;
+            Maeland.NextVillage = Schenig;
+            Schenig.NextVillage = Alst;
+        }
+
+        public void WalkAround()
+        {
+            Village CurrentVillage = this.Maeland;
+            if (CurrentVillage.isAstrildeHere)
+            {
+                Console.WriteLine("Hugi found Astrilde!!! celebrations! XOXO");
+            }
+            Village NextVillage;
+            NextVillage = CurrentVillage.NextVillage;
+
+        }
     }
 
+    class Village
 
-    class village
     {
-       village PreviousNode;
-        village NextNode;
-        string VillageName;
-        bool isAstrildeHere = false;
-        public  village() { }
+       
+        public Village PreviousVillage;
+        public Village NextVillage;
+        public String VillageName;
+        public bool isAstrildeHere = false;
+        public Village() { }
     }
 }
